449e0bff739a515a957406d1a0ce3863bbfb80a5
Fedulova Anastasiia <nasikfed@yandex.ru>
2024-12-06 19:43:08 +0400

    сверстала экран

--- ImageFeed/ImagesList/ProfileViewController.swift
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -12,14 +12,76 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let avatar = UIImage(named: "UsersAvatar")
+        let usersAvatar = UIImageView(image: avatar)
+        usersAvatar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usersAvatar)
+        usersAvatar.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        usersAvatar.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        usersAvatar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        usersAvatar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+
+        
+        let usersName = UILabel()
+        usersName.text = "Екатерина Новикова"
+        usersName.textColor = .white
+        usersName.font = .boldSystemFont(ofSize: 23)
+        usersName.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usersName)
+        usersName.topAnchor.constraint(equalTo: usersAvatar.bottomAnchor, constant: 8).isActive = true
+        usersName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        usersName.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 16).isActive = true
     
-    @IBOutlet var UsersAvatar: UIImageView!
-    @IBOutlet var UsersName: UILabel!
-    @IBOutlet var UsersEmail: UILabel!
-    @IBOutlet var UsersText: UILabel!
-    @IBOutlet var ExitButton: UIButton!
-    
-    @IBAction func TupToExitButton() {
+        
+        
+        
+        
+        let usersEmail = UILabel()
+        usersEmail.text = "@ekaterina_nov"
+        usersEmail.textColor = .ypGray
+        usersEmail.font = .systemFont(ofSize: 13)
+        usersEmail.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usersEmail)
+        usersEmail.topAnchor.constraint(equalTo: usersName.bottomAnchor, constant: 8).isActive = true
+        usersEmail.leadingAnchor.constraint(equalTo: usersName.leadingAnchor).isActive = true
+        usersEmail.trailingAnchor.constraint(equalTo: usersName.trailingAnchor).isActive = true
+        
+        
+        
+        let usersText = UILabel()
+        usersText.text = "Hello, world!"
+        usersText.textColor = .white
+        usersText.font = .systemFont(ofSize: 13)
+        usersText.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usersText)
+        usersText.topAnchor.constraint(equalTo: usersEmail.bottomAnchor, constant: 8).isActive = true
+        usersText.leadingAnchor.constraint(equalTo: usersEmail.leadingAnchor).isActive = true
+        usersText.trailingAnchor.constraint(equalTo: usersEmail.trailingAnchor).isActive = true
+        
+        
+        let button = UIButton.systemButton(
+                    with: UIImage(named: "exit")!,
+                    target: self,
+                    action: #selector(Self.didTapButton)
+                )
+        
+        button.tintColor = .ypRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        button.centerYAnchor.constraint(equalTo: usersAvatar.centerYAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
     }
     
-}
+    
+    @objc
+    private func didTapButton() {
+    }
+    }
+
